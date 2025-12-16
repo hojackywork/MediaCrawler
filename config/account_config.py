941a5e6d7ce929a03651557568dded2c61079eb2
hojackywork <hojacky.work@gmail.com>
2025-12-16 23:53:25 +0800

    Create account_config.py

---
+++ config/account_config.py
@@ -0,0 +1,13 @@
+# account_config.py
+# 這裡存放所有平台的 Cookie
+
+COOKIES = {
+    # 小紅書 Cookie (web_session 值)
+    'xhs': 'a1=198c692c80a5r7t0c4t0fwl014twpfrzr9k1vbnsh30000106703; webId=9e92d1176cf74a57da6caa97fa1c7ae5; gid=yjYSKjJi2jFiyjYSKjJSYjDI802FW98S498iTMxE18y49yq8EUiFuh888y8KW8q8J2WSDiWD; abRequestId=9e92d1176cf74a57da6caa97fa1c7ae5; unread={%22ub%22:%226921a3c2000000001e033f1e%22%2C%22ue%22:%226931d2be000000001e03a0b9%22%2C%22uc%22:28}; xsecappid=xhs-pc-web; web_session=040069b1b9cdca560a7d92b00b3b4bcfa3eb08; webBuild=5.0.7; websectiga=634d3ad75ffb42a2ade2c5e1705a73c845837578aeb31ba0e442d75c648da36a; sec_poison_id=9aa7a500-790e-4df4-a591-d47f891a4dbb; acw_tc=0ad62dda17658792710197303e96bdb15168c77e43aea33f64559b51638434; loadts=1765879288150',
+    
+    # 抖音 Cookie (完整 Cookie 字串)
+    'dy': '你的_douyin_cookie_字串...',
+    
+    # 快手 Cookie (完整 Cookie 字串)
+    'ks': '你的_kuaishou_cookie_字串...'
+}
